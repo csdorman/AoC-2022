5cd04ba08feac125a4e21cb8adda57a352dd24d2
Colin Dorman <csdorman@gmail.com>
2022-12-26 21:15:49 -0500

    Day 5
    Spacing

--- script.js
+++ script.js
@@ -6489,7 +6489,7 @@ move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2`
-
+// Split raw data into crate and instructions
 const day5Split = day5TestRaw.split('\n\n')
 
 const day5Data = day5Split[0]
